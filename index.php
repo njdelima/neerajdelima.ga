8681fac92d0376aed35fbf9bf94426b9202a5a54
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-17 14:10:37 -0500

    location of photo

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <body>
 	<div class="main-container">
 		<h1>Neeraj J. DeLima</h1>
+		<img src="photo.jpg" alt="Neeraj DeLima headshot" class="main-photo" />
 		<p>
 			Hello! My name's Neeraj DeLima and I'm a young, aspiring software developer, entrepreneur and dreamer whose
 			aim in life is to disrupt the status quo. I grew up in Mumbai, India but my experiences and good fortune have taken me
@@ -26,6 +27,5 @@
 		<p>
 			Thanks for visiting my website! Keep scrolling to check out the rest of it and shoot me a message below.
 		</p>
-		<img src="photo.jpg" alt="Neeraj DeLima headshot" class="main-photo" />
 	</div>
 </body>
