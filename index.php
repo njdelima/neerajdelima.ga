77a5ba50b8d98caaa543b59f7d1c1d17a1155f09
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-17 13:01:33 -0500

    width

--- index.php
+++ index.php
@@ -9,20 +9,22 @@
 	<link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 <body>
-	<h1>Neeraj J. DeLima</h1>
-	<p>
-		Hello! My name's Neeraj DeLima and I'm a young, aspiring software developer, entrepreneur and dreamer whose
-		aim in life is to disrupt the status quo. I grew up in Mumbai, India but my experiences and good fortune have taken me
-		far away, to Atlanta, GA where I'm currently based. I have a certain curiosity about most things in the universe, from
-		astronomy to politics, which is something I cherish. I'm not one to turn down an opportunity to learn something new, no
-		matter what it is.
-	</p>
-	<p>
-		I'm enrolled at the Georgia Institute of Technology studying Computer Science, and expected to graduate in 2018.
-		My hobbies include playing and following sports, learning Mandarin Chinese, teaching myself jazz piano and attempting
-		to watch every film made between 1967 and 1975. I'm a huge fan of the Atlanta Hawks and Georgia Tech basketball.
-	</p>
-	<p>
-		Thanks for visiting my website! Keep scrolling to check out the rest of it and shoot me a message below.
-	</p>
+	<div class="main-container">
+		<h1>Neeraj J. DeLima</h1>
+		<p>
+			Hello! My name's Neeraj DeLima and I'm a young, aspiring software developer, entrepreneur and dreamer whose
+			aim in life is to disrupt the status quo. I grew up in Mumbai, India but my experiences and good fortune have taken me
+			far away, to Atlanta, GA where I'm currently based. I have a certain curiosity about most things in the universe, from
+			astronomy to politics, which is something I cherish. I'm not one to turn down an opportunity to learn something new, no
+			matter what it is.
+		</p>
+		<p>
+			I'm enrolled at the Georgia Institute of Technology studying Computer Science, and expected to graduate in 2018.
+			My hobbies include playing and following sports, learning Mandarin Chinese, teaching myself jazz piano and attempting
+			to watch every film made between 1967 and 1975. I'm a huge fan of the Atlanta Hawks and Georgia Tech basketball.
+		</p>
+		<p>
+			Thanks for visiting my website! Keep scrolling to check out the rest of it and shoot me a message below.
+		</p>
+	</div>
 </body>
