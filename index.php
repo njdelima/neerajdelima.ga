75a73ac03f0ff01601674ddb507b2df18c6309a4
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-17 12:37:27 -0500

    Placeholder texts + fonts

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="description" content="" />
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 <body>
-	<p>Neeraj J. DeLima</p>
+	<h1>Neeraj J. DeLima</h1>
+	<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultricies laoreet tempor. Vivamus tempor diam non erat volutpat, eu cursus turpis suscipit. Curabitur rhoncus vestibulum erat, tempus rutrum velit rutrum at. Duis accumsan sapien tortor, sit amet vulputate purus bibendum tempor. Phasellus dapibus mauris congue, posuere augue ut, tristique nibh. Sed pellentesque ultricies odio, quis pellentesque velit aliquam quis. Aenean id hendrerit metus. Etiam id nulla bibendum, tristique mauris eu, efficitur nisi. </p>
 	
 </body>
