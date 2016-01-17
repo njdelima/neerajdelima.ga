a767e0b35307230911a6e9fed697a367498a28a0
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-17 12:40:27 -0500

    trying new fonts

--- index.php
+++ index.php
@@ -8,11 +8,10 @@
 	<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 <body>
 	<h1>Neeraj J. DeLima</h1>
 	<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultricies laoreet tempor. Vivamus tempor diam non erat volutpat, eu cursus turpis suscipit. Curabitur rhoncus vestibulum erat, tempus rutrum velit rutrum at. Duis accumsan sapien tortor, sit amet vulputate purus bibendum tempor. Phasellus dapibus mauris congue, posuere augue ut, tristique nibh. Sed pellentesque ultricies odio, quis pellentesque velit aliquam quis. Aenean id hendrerit metus. Etiam id nulla bibendum, tristique mauris eu, efficitur nisi. </p>
-	
+
 </body>
