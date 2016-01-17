46d8ce19198f2a649d4995b34ee2ab241cea2562
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-17 12:30:24 -0500

    Basic scaffolding and <head> tag

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="" />
+</head>
+<body>
+	<p>Neeraj J. DeLima</p>
+	
+</body>
