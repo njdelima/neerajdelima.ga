f40958e2e483577fe8ab42b37cef49dd87cece6a
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-01-21 20:04:14 -0500

    actually changed the effect

--- index.php
+++ index.php
@@ -232,9 +232,9 @@
 
 			});
 			$(".project-thumbnail").mouseenter(function() {
-				$(this).children(".thumbnail-overlay").show("slide",{direction:"top"}, 400);
+				$(this).children(".thumbnail-overlay").slideDown(400);
 			}).mouseleave(function() {
-				$(this).children(".thumbnail-overlay").hide("slide",{direction:"top"}, 400);
+				$(this).children(".thumbnail-overlay").slideUp(400);
 			});
 
 		});
