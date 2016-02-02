57ac2212ad795d0fef9d9923ba198e4e60b32e31
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-02-02 16:27:55 -0500

    added icon and title

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
+	<link href="photo.jpg" type="image/jpeg" rel="icon" />
+	<title>Neeraj J. DeLima</title>
+
 	<link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 <body>
